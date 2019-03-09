5326acbe953103dc6a5557069a6c23ee790445d5
mateuszp <mpftp10@gmail.com>
2019-03-09 18:55:35 +0100

    backend mvn init

---
+++ backend/src/main/java/com/zpi/teache/TeacheApplication.java
@@ -0,0 +1,13 @@
+package com.zpi.teache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacheApplication.class, args);
+	}
+
+}
