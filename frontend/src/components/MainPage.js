2685489173b8c4a421ff57b2c545711bc769ceb9
Monteth <greezlie@gmail.com>
2019-03-13 18:48:51 +0100

    Added messages

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -2,6 +2,9 @@ import React, {Component} from "react"
 import ChatGroup from './ChatGroup'
 import bgPic from "../assets/mntnFHD_compressed_cut.jpeg";
 import TLogo from '../assets/TLogo_cut.png'
+import Photo from '../assets/photo.png'
+import Send from '../assets/send.png'
+import File from '../assets/file.png'
 import '../styles/MainPage.scss'
 
 
@@ -11,27 +14,57 @@ class MainPage extends Component {
         super(props);
         this.state = {
             groups: [
-                { id: 345, url: 'https://randomuser.me/api/portraits/med/women/21.jpg'},
-                { id: 243, url: 'https://randomuser.me/api/portraits/med/men/56.jpg'},
-                { id: 834, url: 'https://randomuser.me/api/portraits/med/men/47.jpg'},
-                { id: 153, url: 'https://randomuser.me/api/portraits/med/women/96.jpg'},
-                { id: 152, url: 'https://randomuser.me/api/portraits/med/women/79.jpg'}
+                {id: 345, url: 'https://randomuser.me/api/portraits/med/women/21.jpg'},
+                {id: 243, url: 'https://randomuser.me/api/portraits/med/men/56.jpg'},
+                {id: 834, url: 'https://randomuser.me/api/portraits/med/men/47.jpg'},
+                {id: 153, url: 'https://randomuser.me/api/portraits/med/women/96.jpg'},
+                {id: 152, url: 'https://randomuser.me/api/portraits/med/women/79.jpg'}
             ],
             nrOfGroups: 4,
             activeGroup: 834,
+            message: 'piszę do ',
+            messages: [
+                {id: 0, message: 'Cześć!', id_sender: 154},
+                {id: 1, message: 'Hej', id_sender: 463},
+                {id: 2, message: 'Co u Ciebie?', id_sender: 154},
+            ]
         };
         // this.groupChanged = this.groupChanged.bind(this)
     }
+
+    handleSend(event){
+        this.setState(prevState =>{
+            const id = prevState.messages[prevState.messages.length -1] +1;
+            prevState.messages.push({
+                    id: id,
+                message: prevState.message,
+                id_sender: 154});
+            prevState.message = '';
+            return prevState
+        });
+        console.log(this.state.messages.length)
+    }
+
+    handleChangeInput(event){
+        const {name, value} = event.target;
+        this.setState({
+            message: value
+        })
+
+    }
+
 
     groupChanged = (id) => {
         this.setState(prevState => {
             return prevState.activeGroup = id
         })
-    }
-
+    };
 
 
     render() {
+        const messagesList = this.state.messages.map(message =>
+                <div>{message.message}</div>
+            );
         const groupsCompList =
             this.state.groups.map(chat =>
                 <ChatGroup
@@ -43,18 +76,18 @@ class MainPage extends Component {
 
         return (
             <div id='chat-body'
-            style={{backgroundImage: `url(${bgPic})`}}>
+                 style={{backgroundImage: `url(${bgPic})`}}>
                 <div className='main-container'>
-                    <div id='groups' >
+                    <div id='groups'>
                         {/*<ChatGroup url={TLogo} />*/}
                         {groupsCompList}
 
                     </div>
                     <div id='chat'>
-                        <div id='chat-head'>
-                            <div className='name'>
-                                <h3>Hannah Reed</h3>
-                            </div>
+                        <div className='chat-head'>
+
+                            <h3>Hannah Reed</h3>
+
 
                             <div className='send-form'>
 
@@ -62,14 +95,20 @@ class MainPage extends Component {
 
                         </div>
                         <div className='messages'>
-                            Chat
+                            {messagesList}
                         </div>
+
                         <div className='send-form'>
-                            Send form
+                            <input value={this.state.message} type='text' onChange={event => this.handleChangeInput(event)} className='form-control'/>
+                            <img src={Photo}/>
+                            <img src={File}/>
+                            <img src={Send}
+                                 onClick={(event) => this.handleSend(event)}/>
                         </div>
                     </div>
                     <div id='details'>
-
+                        {/*DETAILS*/}
+                        {this.state.message}
                     </div>
                 </div>
             </div>
