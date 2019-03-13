fe9e3258f7be506b67632a968d70e8dc07f1f864
mateuszp <mpftp10@gmail.com>
2019-03-13 23:59:05 +0100

    enabled app properties

--- backend/src/main/java/com/zpi/teache/TeacheApplication.java
+++ backend/src/main/java/com/zpi/teache/TeacheApplication.java
@@ -1,9 +1,12 @@
 package com.zpi.teache;
 
+import com.zpi.teache.Config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class TeacheApplication {
 
 	public static void main(String[] args) {
