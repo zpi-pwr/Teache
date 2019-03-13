97e20faa8e5afd060ba3ec776a01edd8329e0fd4
mateuszp <mpftp10@gmail.com>
2019-03-14 00:46:49 +0100

    declared static fields

--- backend/src/main/java/com/zpi/teache/Security/OAuth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ backend/src/main/java/com/zpi/teache/Security/OAuth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -1,4 +1,12 @@
 package com.zpi.teache.Security.OAuth2;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HttpCookieOAuth2AuthorizationRequestRepository {
+
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+    private static final int cookieExpireSeconds = 180;
+
 }
