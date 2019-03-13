83426f740be5faf0f3ff5216ca86a4e69e3d5f17
mateuszp <mpftp10@gmail.com>
2019-03-14 00:13:42 +0100

    added annotations

--- backend/src/main/java/com/zpi/teache/Config/SecurityConfig.java
+++ backend/src/main/java/com/zpi/teache/Config/SecurityConfig.java
@@ -1,4 +1,15 @@
 package com.zpi.teache.Config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig {
 }
