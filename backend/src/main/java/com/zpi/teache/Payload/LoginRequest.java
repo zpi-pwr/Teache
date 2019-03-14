abf4fb180ca45f5c9fcd7822cc82c8b50ec6fc34
mateuszp <mpftp10@gmail.com>
2019-03-14 01:42:25 +0100

    defined login request class

---
+++ backend/src/main/java/com/zpi/teache/Payload/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.zpi.teache.Payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    // Getters and Setters (Omitted for brevity)
+}
