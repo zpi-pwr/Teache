705533584274f28286e0329a6d1db1a39933660f
mateuszp <mpftp10@gmail.com>
2019-03-14 01:45:44 +0100

    defined auth response

---
+++ backend/src/main/java/com/zpi/teache/Payload/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.zpi.teache.Payload;
+
+public class AuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    // Getters and Setters (Omitted for brevity)
+}
