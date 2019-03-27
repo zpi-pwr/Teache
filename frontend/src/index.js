d6c5c117b22d27e517961f11e78b875f8c056c2a
Monteth <greezlie@gmail.com>
2019-03-27 21:10:16 +0100

    Add redux to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/bootstrap.min.css'
+import {Provider} from "react-redux";
+import {store} from "./reducers/store";
 
-ReactDOM.render(<App style={{height: '100%'}} />, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={ store }><App style={{height: '100%'}} /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
