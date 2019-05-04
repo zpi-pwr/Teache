342426f475c1fb825bdde1ff019a6e80630d56de
eipc16 <pprzemek.312@gmail.com>
2019-05-04 15:18:48 +0200

    finished token contract

--- contracts/ERC20_Token.sol
+++ contracts/ERC20_Token.sol
@@ -86,3 +86,75 @@ contract Owned {
         newOwner = address(0);
     }
 }
+
+
+// ----------------------------------------------------------------------------
+// Teache Coin - Code
+// Interfaces: ERC20Interface, Owner, SafeMath
+// ----------------------------------------------------------------------------
+contract TeacheCoin is ERC20Interface, Owner, SafeMath {
+    string public symbol;
+    string public fullName;
+    uint8 public decimals;
+    uint public _totalSupply;
+
+    mapping(address => uint) userBalances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() public {
+        symbol = "ThC";
+        name = "TeacheCoin";
+        decimals = 18;
+        _totalSupply = 100000000000000000000000000;
+        // userBalances[OnwerAddress] = _totalSupply;
+        // emit Transfer(address(0), OwnerAddress, _totalSupply);
+    }
+
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return userBalances[tokenOwner];
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = sub(balances[msg.sender], tokens);
+        balances[to] = add(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = sub(balances[from], tokens);
+        allowed[from][msg.sender] = sub(allowed[from][msg.sender], tokens);
+        balances[to] = add(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }    
+
+    public allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+
+    function () public payable {
+        revert();
+    }
+
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+}
